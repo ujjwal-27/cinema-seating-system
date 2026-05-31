7b868e6bb044c1d200652c38bcc53f19e910c4da
Ujjwal Shrestha <ujjwalshresth.96@gmail.com>
2026-05-31 21:05:49 +0100

    update seat grid

--- src/components/SeatGrid.tsx
+++ src/components/SeatGrid.tsx
@@ -1,34 +1,54 @@
 import { cinemaSeats } from "../data/cinemaLayout";
 
 function SeatGrid() {
+  const rows = [...new Set(cinemaSeats.map((seat) => seat.row))];
+
   return (
     <div>
       <h2>Cinema Seating Layout</h2>
 
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "6px",
-        }}
-      >
-        {cinemaSeats.map((seat) => (
+      {rows.map((row) => {
+        const rowSeats = cinemaSeats.filter(
+          (seat) => seat.row === row
+        );
+
+        return (
           <div
-            key={seat.id}
+            key={row}
             style={{
-              width: "50px",
-              height: "50px",
-              border: "1px solid black",
               display: "flex",
-              justifyContent: "center",
+              gap: "6px",
+              marginBottom: "8px",
               alignItems: "center",
-              borderRadius: "4px",
             }}
           >
-            {seat.id}
+            <strong
+              style={{
+                width: "30px",
+              }}
+            >
+              {row}
+            </strong>
+
+            {rowSeats.map((seat) => (
+              <div
+                key={seat.id}
+                style={{
+                  width: "40px",
+                  height: "40px",
+                  border: "1px solid black",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "4px",
+                }}
+              >
+                {seat.number}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        );
+      })}
     </div>
   );
 }
