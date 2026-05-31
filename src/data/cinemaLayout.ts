c16dadd9d39c0a6946968cd02e2007a2137e29fd
Ujjwal Shrestha <ujjwalshresth.96@gmail.com>
2026-05-31 21:31:37 +0100

    update cinema layout

--- src/data/cinemaLayout.ts
+++ src/data/cinemaLayout.ts
@@ -1,16 +1,61 @@
 import type { Seat } from "../models/Seat";
 import { SeatStatus, SeatType } from "../models/Seat";
 
-const rowConfiguration: Record<string, number[]> = {
-  A: [1, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 27, 28],
+const rowConfiguration: Record<string, (number | null)[]> = {
+  A: [
+    1, 2,
+    null, null,
+    5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16,
+    17, 18, 19, 20, 21, 22,
+    23, 24,
+    null, null,
+    27, 28,
+],
 
-  B: Array.from({ length: 28 }, (_, i) => i + 1),
+  B: [
+    1, 2, 3, 4,
+    null,
+    5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16,
+    17, 18, 19, 20, 21, 22,
+    23, 24,
+    null,
+    25, 26, 27, 28,
+],
 
-  C: Array.from({ length: 28 }, (_, i) => i + 1),
+    C: [
+    1, 2, 3, 4,
+    null,
+    5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16,
+    17, 18, 19, 20, 21, 22,
+    23, 24,
+    null,
+    25, 26, 27, 28,
+    ],
 
-  D: Array.from({ length: 28 }, (_, i) => i + 1),
+    D: [
+    1, 2, 3, 4,
+    null,
+    5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16,
+    17, 18, 19, 20, 21, 22,
+    23, 24,
+    null,
+    25, 26, 27, 28,
+    ],
 
-  E: Array.from({ length: 28 }, (_, i) => i + 1),
+    E: [
+    1, 2, 3, 4,
+    null,
+    5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16,
+    17, 18, 19, 20, 21, 22,
+    23, 24,
+    null,
+    25, 26, 27, 28,
+    ],
 
   F: Array.from({ length: 28 }, (_, i) => i + 1),
 
@@ -38,13 +83,17 @@ export function generateCinemaLayout(): Seat[] {
 
   Object.entries(rowConfiguration).forEach(([row, seatNumbers]) => {
     seatNumbers.forEach((seatNumber) => {
-      seats.push({
-        id: `${row}${seatNumber}`,
-        row,
-        number: seatNumber,
-        type: SeatType.STANDARD,
-        status: SeatStatus.AVAILABLE,
-      });
+        if (seatNumber === null) {
+            return;
+        }
+        
+        seats.push({
+            id: `${row}${seatNumber}`,
+            row,
+            number: seatNumber,
+            type: SeatType.STANDARD,
+            status: SeatStatus.AVAILABLE,
+        });
     });
   });
 
