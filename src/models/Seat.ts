0757a5a1cd78f8d0a6dee0d7f3118370c55c3025
Ujjwal Shrestha <ujjwalshresth.96@gmail.com>
2026-05-31 20:31:24 +0100

    add Seat model

---
+++ src/models/Seat.ts
@@ -0,0 +1,23 @@
+export const SeatType = {
+  STANDARD: "STANDARD",
+  VIP: "VIP",
+  ACCESSIBILITY: "ACCESSIBILITY",
+} as const;
+
+export type SeatType = (typeof SeatType)[keyof typeof SeatType];
+
+export const SeatStatus = {
+  AVAILABLE: "AVAILABLE",
+  BOOKED: "BOOKED",
+  BROKEN: "BROKEN",
+} as const;
+
+export type SeatStatus = (typeof SeatStatus)[keyof typeof SeatStatus];
+
+export interface Seat {
+  id: string;
+  row: string;
+  number: number;
+  type: SeatType;
+  status: SeatStatus;
+}
