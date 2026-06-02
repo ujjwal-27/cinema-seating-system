17e133c11ae7f4c1f2f565691ce833a93e4020a1
Ujjwal Shrestha <ujjwalshresth.96@gmail.com>
2026-06-02 19:58:25 +0100

    fix code structure

--- src/models/Seat.ts
+++ src/models/Seat.ts
@@ -1,24 +1,24 @@
 export const SeatType = {
-  STANDARD: "STANDARD",
-  VIP: "VIP",
-  ACCESSIBILITY: "ACCESSIBILITY",
+    STANDARD: "STANDARD",
+    VIP: "VIP",
+    ACCESSIBILITY: "ACCESSIBILITY",
 } as const;
 
 export type SeatType = (typeof SeatType)[keyof typeof SeatType];
 
 export const SeatStatus = {
-  AVAILABLE: "AVAILABLE",
-  SELECTED: "SELECTED",
-  BOOKED: "BOOKED",
-  BROKEN: "BROKEN",
+    AVAILABLE: "AVAILABLE",
+    SELECTED: "SELECTED",
+    BOOKED: "BOOKED",
+    BROKEN: "BROKEN",
 } as const;
 
 export type SeatStatus = (typeof SeatStatus)[keyof typeof SeatStatus];
 
 export interface Seat {
-  id: string;
-  row: string;
-  number: number;
-  type: SeatType;
-  status: SeatStatus;
+    id: string;
+    row: string;
+    number: number;
+    type: SeatType;
+    status: SeatStatus;
 }
